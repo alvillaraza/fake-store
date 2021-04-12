978c1aeec55ca33819d6e2b6f31dc24b02156c48
Alexis Villaraza <alvillaraza@Alexiss-MacBook-Air.local>
2021-04-12 10:46:09 -0700

    added add to cart and pricing to store.js

--- fake-store/src/components/ProductCard.js
+++ fake-store/src/components/ProductCard.js
@@ -1,11 +1,14 @@
 import React from "react";
+import AddToCartButton from "./AddToCart";
 
 function ProductCard({ product }) {
 
   return (
     <div className="product-card">
-      <h2>{product.title}</h2>
       <img src={product.image} alt="product thumbnail" />
+      <h5>{product.title}</h5>
+      <h4>${(product.price).toFixed(2)}</h4>
+      <AddToCartButton id={product.id} />
     </div>
   );
 }
